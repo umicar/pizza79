cf62b19ed7233cc8e0f4427862ee2b53352a2903
umicar <77049691+umicar@users.noreply.github.com>
2021-05-04 14:26:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+menu_list_array = ["Chicken Tandoori Pizza","Veg Supreme pizza","Paneer Tikka Pizza","Deluxe Veggie pizza"];
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menu_list_array'>";
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>'+menu_list_array[i]
+    }
+    htmldata=htmldata+ "</ol>";
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>";
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>'+menu_list_array[i]
+    }
+    htmldata=htmldata+"</section>";
+     document.getElementById("display_addedmenu").innerHTML=htmldata;
+}
